505e0b87b3011c7351ab9112d12adddc3d9445d5
kpraveenkumar <kpraveenkumar89@ymail.com>
2020-03-26 13:01:23 +0100

    Refactor using latest c++17 approach

--- src/check_equal_without_relational_operator.cpp
+++ src/check_equal_without_relational_operator.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 int main() {
-  int x, y;
-  cin >> x >> y;
-  if (!(x ^ y)) {
+  if (int x,y;cin >> x >> y&&!(x ^ y)) {
     cout << x << " and " << y << " are equal" << endl;
   } else {
     cout << x << " and " << y << " are not equal" << endl;
